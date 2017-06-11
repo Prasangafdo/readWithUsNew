cdda39b274783a79435afb3b59d62cf92ed6269d
Prasanga <prasangafdz@outlook.com>
2017-06-12 04:51:23 +0530

    Working on member table. Search is working. Update is not working. Need to test. Delete not teested yet.

--- JavaAssignment/src/javaassignment/MemberSearchNew.java
+++ JavaAssignment/src/javaassignment/MemberSearchNew.java
@@ -5,6 +5,13 @@
  */
 package javaassignment;
 
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static javaassignment.MemberReg.Updater;
+import static javaassignment.MemberReg.Search;
+import javax.swing.JOptionPane;
+import net.proteanit.sql.DbUtils;
 /**
  *
  * @author Prasanga Fernando
@@ -32,11 +39,15 @@ public class MemberSearchNew extends javax.swing.JFrame {
         memberreg_1Query = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT m FROM Memberreg_1 m");
         memberreg_1List = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : memberreg_1Query.getResultList();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tblMemberdata = new javax.swing.JTable();
+        txtSearchBox = new javax.swing.JTextField();
+        btnSearch = new javax.swing.JButton();
+        btnUpdate = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        org.jdesktop.swingbinding.JTableBinding jTableBinding = org.jdesktop.swingbinding.SwingBindings.createJTableBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, memberreg_1List, jTable1);
+        org.jdesktop.swingbinding.JTableBinding jTableBinding = org.jdesktop.swingbinding.SwingBindings.createJTableBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, memberreg_1List, tblMemberdata);
         org.jdesktop.swingbinding.JTableBinding.ColumnBinding columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${memberid}"));
         columnBinding.setColumnName("Memberid");
         columnBinding.setColumnClass(Integer.class);
@@ -60,7 +71,38 @@ public class MemberSearchNew extends javax.swing.JFrame {
         columnBinding.setColumnClass(String.class);
         bindingGroup.addBinding(jTableBinding);
 
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(tblMemberdata);
+
+        txtSearchBox.setText("Enter member name here");
+        txtSearchBox.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtSearchBoxMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                txtSearchBoxMouseEntered(evt);
+            }
+        });
+
+        btnSearch.setText("Search");
+        btnSearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSearchActionPerformed(evt);
+            }
+        });
+
+        btnUpdate.setText("Update");
+        btnUpdate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUpdateActionPerformed(evt);
+            }
+        });
+
+        btnDelete.setText("Delete");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -70,6 +112,20 @@ public class MemberSearchNew extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(txtSearchBox, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(53, 53, 53)
+                            .addComponent(btnSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(50, 50, 50)
+                            .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(100, 100, 100)
+                            .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -77,12 +133,80 @@ public class MemberSearchNew extends javax.swing.JFrame {
                 .addContainerGap(94, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(66, 66, 66))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(txtSearchBox, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(180, 180, 180)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
 
         bindingGroup.bind();
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtSearchBoxMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtSearchBoxMouseClicked
+        // TODO add your handling code here:
+        //txtSearchBox.selectAll();
+        txtSearchBox.setText("");//To make it null on click
+    }//GEN-LAST:event_txtSearchBoxMouseClicked
+
+    private void txtSearchBoxMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtSearchBoxMouseEntered
+        txtSearchBox.selectAll();
+    }//GEN-LAST:event_txtSearchBoxMouseEntered
+
+    private void btnSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSearchActionPerformed
+        String sql = "SELECT * FROM memberreg WHERE `Fname` = '"+ txtSearchBox.getText()+"'|| `Lname`= '"+ txtSearchBox.getText()+"'";
+
+        try {
+            ResultSet rset=  Search(sql);
+            tblMemberdata.setModel(DbUtils.resultSetToTableModel(rset));
+        } catch (Exception ex) {
+            Logger.getLogger(MemberSearch.class.getName()).log(Level.SEVERE, null, ex);//auto gen one
+
+        }
+    }//GEN-LAST:event_btnSearchActionPerformed
+
+    private void btnUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUpdateActionPerformed
+        String MemberID = tblMemberdata.getValueAt(0, 0).toString();
+        int MID = Integer.valueOf((String) MemberID);
+        String Fname =    tblMemberdata.getValueAt(0, 1).toString();
+        String Lname =   tblMemberdata.getValueAt(0, 2).toString();
+        String Birthday = tblMemberdata.getValueAt(0, 3).toString();
+        String Address =  tblMemberdata.getValueAt(0, 4).toString();
+        String NIC =     tblMemberdata.getValueAt(0, 5).toString();
+        String Gender =   tblMemberdata.getValueAt(0, 6).toString();
+
+        String sql = "UPDATE `memberreg` SET `Fname`='"+Fname+"', `Lname`='"+Lname+"', `dob` = '"+Birthday+"',  `Address` = '"+Address+"', `NIC` = '"+NIC+"', `Gender` = '"+Gender+"' where MemberID = '"+MID+"'";
+
+        try{
+            Updater(sql);
+            JOptionPane.showMessageDialog(null, "Values are updated");
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }//GEN-LAST:event_btnUpdateActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        String MemberID = tblMemberdata.getValueAt(0, 0).toString();
+        int MID = Integer.valueOf((String) MemberID);
+        String sql = "DELETE from `memberreg` WHERE `MemberID` = '"+MID+"'";
+        try{
+            Updater(sql);
+            JOptionPane.showMessageDialog(null, "Value deleted successfully");
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+
+    }//GEN-LAST:event_btnDeleteActionPerformed
 
     /**
      * @param args the command line arguments
@@ -120,11 +244,15 @@ public class MemberSearchNew extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnSearch;
+    private javax.swing.JButton btnUpdate;
     private javax.persistence.EntityManager entityManager;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private java.util.List<javaassignment.Memberreg_1> memberreg_1List;
     private javax.persistence.Query memberreg_1Query;
+    private javax.swing.JTable tblMemberdata;
+    private javax.swing.JTextField txtSearchBox;
     private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 }
